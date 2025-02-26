cdd690357072cc8f380162fdc3b12e69751c9420
Analia Arias <analia.arias@wbdcontractor.com>
2025-02-26 03:28:01 +0100

    feature(search): add search functionality

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ import { useQuery } from '@tanstack/react-query';
 import { useMarvelStore } from '@/app/store/store';
 import CharacterCard from '@/app/components/character';
 
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { useSearchParams } from "next/navigation";
 
 const fetchCharacters= async function () {
   const response = await fetch('/api/characters');
@@ -15,7 +16,10 @@ const fetchCharacters= async function () {
 export default function Home() {
 
   const { characters, setCharacters } = useMarvelStore();
+  const [searchTerm, setSearchTerm] = useState('');
   const memoizedCharacters = useMemo(() => characters, [characters]);
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get('search') || '';
 
   const { data, isLoading, error } = useQuery({
     queryKey: ['results'],
@@ -29,15 +33,33 @@ export default function Home() {
     }
   }, [data, setCharacters]);
 
+  useEffect(() => {
+    setSearchTerm(searchQuery);
+  }, [searchQuery]);
+
+  const filteredCharacters = memoizedCharacters.filter(character =>
+    character.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Error loading data</p>;
 
   return (
     <main className={styles.main} aria-labelledby='title' tabIndex={-1}>
-      <h1 id="title" className={styles.visuallyHidden}>Marvel Characters</h1>
+      <h1 id="title" className="visuallyHidden">Marvel Characters</h1>
+      <div className={styles.searchContainer}>
+        <label htmlFor="search" className="visuallyHidden">Search by character name</label>
+        <input
+          type="text"
+          aria-label="Search by character name"
+          placeholder="SEARCH A CHARACTER..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className={styles.searchInput} />
+        <legend className={styles.searchResults}>{filteredCharacters.length} results</legend>
+      </div>
       <ul className={styles.charactersContainer}>
-        {memoizedCharacters.map((character) => {
+        {filteredCharacters.map((character) => {
           return (
             <CharacterCard
               id={character.id}
